9a18e5da91b38013f429fcdde23cdfd11b0fc9c3
Zdravko Georgiev <zzgzdravko@gmail.com>
2025-09-12 16:21:16 +0300

    removed old data types folder

--- data types/04.js
+++
@@ -1,9 +0,0 @@
-function solve(a, b, c){
-    let sum = a+b+c;
-
-    const type = Number.isInteger(sum) === true? "Integer":"Float";
-
-    return `${sum} - ${type}`;
-}
-
-console.log(solve(9, 100, 1.1));
