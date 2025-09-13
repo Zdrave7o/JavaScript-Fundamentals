2162e2c1f1ccc5ad5f9e477c4b6dfb892bf5e014
Zdravko Georgiev <zzgzdravko@gmail.com>
2025-09-14 00:31:35 +0300

    dataTypesMoreExercise started. 01digitWithWords.js solved!

---
+++ dataTypesMoreExercise/01digitWithWords.js
@@ -0,0 +1,26 @@
+function showDigit(str){
+    switch(str){
+        case "zero":
+            return 0;
+        case "one":
+            return 1;
+        case"two":
+            return 2;
+        case "three":
+            return 3;
+        case "four":
+            return 4;
+        case "five":
+            return 5;
+        case "six":
+            return 6;
+        case "seven":
+            return 7;
+        case "eight":
+            return 8;
+        case "nine":
+            return 9;
+    }
+}
+
+console.log(showDigit("zero"));
