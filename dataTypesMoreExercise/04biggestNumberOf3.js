961131e35aa3c09d11e85c995c9d316172487e6a
Zdravko Georgiev <zzgzdravko@gmail.com>
2025-09-14 21:13:49 +0300

    04biggestNumberOf3.js solved!

---
+++ dataTypesMoreExercise/04biggestNumberOf3.js
@@ -0,0 +1,15 @@
+function printBiggestNumber(a, b, c){
+    const numbers = [a, b, c];
+    let biggestNum = numbers.sort((x, y) =>  y - x)[0];
+
+    console.log(biggestNum);
+    
+
+}
+
+printBiggestNumber(43,
+
+43.2,
+
+43.1)
+
