18c778e91003710276e223d936a01ab0b0188e94
Zdravko Georgiev <zzgzdravko@gmail.com>
2025-10-01 12:30:40 +0300

    equalSums.js solved!

---
+++ arraysExercise/equalSums.js
@@ -0,0 +1,28 @@
+function equalSums(arr){
+    for(let i = 0; i < arr.length; i++){
+        let leftSum = 0;
+        let rightSum = 0;
+
+        for(let j = 0; j < i; j++){
+            leftSum+=arr[j]
+        }
+
+        for(let j = i + 1; j < arr.length; j++){
+            rightSum+=arr[j]
+        }
+
+        if(leftSum === rightSum){
+            console.log(i);
+            return;
+        }
+    }
+
+    console.log("no");
+    
+}
+
+equalSums([10, 5, 5, 99,
+
+3, 4, 2, 5, 1,
+
+1, 4]);
